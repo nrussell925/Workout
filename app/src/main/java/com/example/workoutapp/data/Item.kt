a4b04b7d3bac4ec934c88a32b642acc547a05750
nrussell925 <nrussell925@gmail.com>
2024-12-03 12:13:10 -0700

    Set up GitHub Pages

---
+++ app/src/main/java/com/example/workoutapp/data/Item.kt
@@ -0,0 +1,4 @@
+package com.example.workoutapp.data
+
+class Item {
+}
---
+++ app/src/main/java/com/example/workoutapp/data/WorkoutDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.workoutapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Database class with a singleton Instance object.
+ */
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class WorkoutDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var Instance: WorkoutDatabase? = null
+
+        fun getDatabase(context: Context): WorkoutDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, WorkoutDatabase::class.java, "item_database")
+                    /**
+                     * Setting this option in your app's database builder means that Room
+                     * permanently deletes all data from the tables in your database when it
+                     * attempts to perform a migration with no defined migration path.
+                     */
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
